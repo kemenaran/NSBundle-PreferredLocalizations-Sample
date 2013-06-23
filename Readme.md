225f354e2fcff0667e628aa6b56b6971e66f83c9
Pierre de La Morinerie <kemenaran@gmail.com>
2013-06-23 15:30:58 +0200

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+This project demonstrates some edge cases in the behavior of `-[NSBundle preferredLocalizationsFromArray:forPreferences:]`.
+
+How to use
+---
+
+As this issue was initially filed against the iOS SDK, it contains a project targeting iOS, under the form of a unit tests bundle.
+
+To demonstrate the issues, execute the `Test` action on the `UnitTests` scheme in the projet.
---
+++ UnitTests.m
@@ -0,0 +1,40 @@
+//
+//  UnitTests.m
+//  UnitTests
+//
+//  Created by Pierre de La Morinerie on 23/06/2013.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface UnitTests : SenTestCase
+@end
+
+@implementation UnitTests
+
+- (void)testExample
+{
+    // Some passing tests
+    STAssertEqualObjects([NSBundle preferredLocalizationsFromArray:(@[@"de", @"en"]) forPreferences:@[@"en"]], @[@"en"],
+                         @"A generic language matches the same generic language");
+
+    STAssertEqualObjects([NSBundle preferredLocalizationsFromArray:(@[@"de", @"en-GB"]) forPreferences:@[@"en-GB"]], @[@"en-GB"],
+                         @"A country variant matches the same country variant");
+    
+    STAssertEqualObjects([NSBundle preferredLocalizationsFromArray:(@[@"de", @"en"]) forPreferences:@[@"en-GB"]], @[@"en"],
+                         @"A country variant in the preferences array fallbacks on the generic language");
+    
+    // Some failing tests, that IMHO should pass
+    STAssertEqualObjects([NSBundle preferredLocalizationsFromArray:(@[@"de", @"en-GB"]) forPreferences:@[@"en-CA"]], @[@"en"],
+                         @"A unavailable country variant should fallback on the generic language"); // returns "de" (unexpected)
+    
+    STAssertEqualObjects([NSBundle preferredLocalizationsFromArray:(@[@"de", @"en-GB"]) forPreferences:@[@"en"]], @[@"en"],
+                         @"An unavailable generic language should fallback on a country variant"); // returns "de" (unexpected)
+    
+    STAssertEquals([[[NSBundle preferredLocalizationsFromArray:(@[@"de", @"en-US"]) forPreferences:@[@"en"]] objectAtIndex:0] hasPrefix:@"en"],
+                   [[[NSBundle preferredLocalizationsFromArray:(@[@"de", @"en-CA"]) forPreferences:@[@"en"]] objectAtIndex:0] hasPrefix:@"en"],
+                   @"The behavior should be consistent between the en-US locale and the other locales"); // (unexpected)
+}
+
+@end
